3be13c9b6698da6f129eb39929ff159058e45969
peterjidamva <jidamvap@gmail.com>
2022-02-25 10:33:32 +0300

    practised a lot

--- app/src/main/java/com/example/java1010/Main2Activity.java
+++ app/src/main/java/com/example/java1010/Main2Activity.java
@@ -1,10 +1,15 @@
 package com.example.java1010;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.service.controls.actions.FloatAction;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import  android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +21,29 @@ public class Main2Activity extends AppCompatActivity {
  protected void onCreate(Bundle savedInstanceState) {
      super.onCreate(savedInstanceState);
      setContentView(R.layout.activity2_main);
+     String txt = "our log";
+     Log.d(txt , "LOGGGGGGGGGGGGGGGGGGGGGG");
+        ImageView foe = (ImageView) findViewById(R.id.imageView) ;
+        foe.setImageResource(R.drawable.bayc);
+
+
+        TextView showme = (TextView) findViewById(R.id.showMe);
+        Bundle extras = getIntent().getExtras();
+
+        if(extras != null ){
+
+            String jina = extras.getString("JIDAMVA");
+
+//            showme.setText(jina);
+
+            Context ctx = Main2Activity.this ;
+
+
+            Toast.makeText(ctx, jina, Toast.LENGTH_LONG).show();
+
+
+
+        }
 
         FloatingActionButton fab1 = (FloatingActionButton) findViewById(R.id.fab1);
         FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.fab2);
@@ -23,27 +51,41 @@ public class Main2Activity extends AppCompatActivity {
 
         fab1.setOnClickListener(
                 v -> {
-                    Intent in = new Intent();
-                    in.setAction(Intent.ACTION_VIEW);
-                    in.setData(Uri.parse("geo:-6.771286929636371, 39.23987923062466"));
+                    Intent in = new Intent(Intent.ACTION_VIEW , Uri.parse("tel:1234"));
+//                    in.setAction(Intent.ACTION_VIEW);
+//                    in.setData(Uri.parse("geo:-6.771286929636371, 39.23987923062466"));
                     startActivity(in);
                 }
+
+
         );
 
         fab2.setOnClickListener(
                 v -> {
-
                     Intent ini = new Intent(Intent.ACTION_VIEW , Uri.parse("https://bayc.com"));
                     startActivity(ini);
                 }
         );
 
         fab3.setOnClickListener(
+                v ->{
+                    System.out.println("clicked");
+                    Context ctx = Main2Activity.this ;
+                    Toast.makeText(ctx, "CLICK", Toast.LENGTH_SHORT).show();
+                }
+        );
+
+        fab3.setOnLongClickListener(
                 v -> {
-                    Intent in1 = new Intent();
-                    in1.setAction(Intent.ACTION_VIEW);
-                    in1.setData(Uri.parse("tel:0755946656"));
-                    startActivity(in1);
+                    System.out.println("ON LONG clicked");
+                    Context ctx = Main2Activity.this ;
+                    Toast.makeText(ctx, "ON LONG CLICK", Toast.LENGTH_SHORT).show();
+
+//                    Intent in1 = new Intent();
+//                    in1.setAction(Intent.ACTION_VIEW);
+//                    in1.setData(Uri.parse("tel:0755946656"));
+//                    startActivity(in1);
+                    return false;
                 }
         );
 
--- app/src/main/java/com/example/java1010/MainActivity.java
+++ app/src/main/java/com/example/java1010/MainActivity.java
@@ -6,16 +6,23 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import android.text.Editable;
 import android.widget.Button;
 
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import android.view.Menu ;
 
 public class MainActivity extends AppCompatActivity {
-
+     
     private  final  String TAG = getClass().getName();
+    private Context mainActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,16 +30,47 @@ public class MainActivity extends AppCompatActivity {
 
 
         Button button= (Button) findViewById(R.id.buttonId);
+        EditText receivedt = (EditText) findViewById(R.id.editTextName);
+//
+//        TextView hhh = (TextView) findViewById(R.id.radbihh);
+//        hhh.getText();
+
+        RadioButton value2 = (RadioButton) findViewById(R.id.radbihh);
+
+       value2.setOnCheckedChangeListener(
+
+               new CompoundButton.OnCheckedChangeListener() {
+                   @Override
+                   public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                   }
+               }
+
+
+       );
+       Editable datagot =  receivedt.getText();
 
         button.setOnClickListener(
                 v -> {
-                    System.out.println("clicked");
-                    Context ctx = MainActivity.this ;
-                    Toast.makeText(ctx, "CLICK", Toast.LENGTH_SHORT).show();
+//                    System.out.println("clicked");
+//                    Context ctx = MainActivity.this ;
+//                    Toast.makeText(ctx, "CLICK", Toast.LENGTH_SHORT).show();
 
-                    Intent i = new Intent(this , Main2Activity.class);
-                    i.putExtra("JIDAMVA", "THIS IS  JIDAMVA");
-                    startActivity(i);
+
+
+                    Intent emailService = new Intent(Intent.ACTION_SEND);
+                    emailService.setData(Uri.parse("mailto:"));
+                    emailService.setType("text/plain");
+                    emailService.putExtra(Intent.EXTRA_EMAIL , "To user");
+                    emailService.putExtra(Intent.EXTRA_SUBJECT ,"Subject here");
+                    emailService.putExtra(Intent.EXTRA_TEXT ,"Your message");
+                    startActivity(emailService);
+                    Context ctx = this.mainActivity ;
+                    Toast.makeText(ctx,"",Toast.LENGTH_SHORT).show();
+//
+//                    Intent i = new Intent(this , Main2Activity.class);
+//                    i.putExtra("JIDAMVA", datagot);
+//                    this.startActivity(i);
 
 
 
